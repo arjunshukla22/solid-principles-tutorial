35d6c58f0d07fe119d08620f178175c36497709a
Arjun Shukla <arjunshukla22@gmail.com>
2025-05-12 17:16:56 +0530

    Project Created

---
+++ solid-principles-tutorial/solid-principles-tutorialTests/solid_principles_tutorialTests.swift
@@ -0,0 +1,17 @@
+//
+//  solid_principles_tutorialTests.swift
+//  solid-principles-tutorialTests
+//
+//  Created by Arjun Shukla on 12/05/25.
+//
+
+import Testing
+@testable import solid_principles_tutorial
+
+struct solid_principles_tutorialTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ solid-principles-tutorial/solid-principles-tutorialUITests/solid_principles_tutorialUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  solid_principles_tutorialUITestsLaunchTests.swift
+//  solid-principles-tutorialUITests
+//
+//  Created by Arjun Shukla on 12/05/25.
+//
+
+import XCTest
+
+final class solid_principles_tutorialUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
