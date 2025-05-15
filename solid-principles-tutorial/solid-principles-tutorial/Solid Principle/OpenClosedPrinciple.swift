f114e6e903a8e368039a6493559c62ff3c3203e9
Arjun Shukla <arjunshukla22@gmail.com>
2025-05-15 09:47:52 +0530

    Add Bad Code of OCP
    Add Bad Code of OCP

--- solid-principles-tutorial/solid-principles-tutorial/Solid Principle/OpenClosedPrinciple.swift
+++ solid-principles-tutorial/solid-principles-tutorial/Solid Principle/OpenClosedPrinciple.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+class APIService {
+    func fetch() -> String {
+        return "Data from API"
+    }
+}
+
+class DatabaseService {
+    func fetch() -> String {
+        return "Data from Database"
+    }
+}
+
+class DataFetcher {
+    func fetchData(source: String) -> String {
+        if source == "api" {
+            return APIService().fetch()
+        } else if source == "db" {
+            return DatabaseService().fetch()
+        } else {
+            return "Unknown source"
+        }
+    }
+}
+
