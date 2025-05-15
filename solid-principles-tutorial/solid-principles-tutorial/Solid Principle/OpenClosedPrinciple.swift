604d83735f562f7a5c6dc03670829d8bf9fee074
Arjun Shukla <arjunshukla22@gmail.com>
2025-05-15 09:57:06 +0530

    Add OCP Violation Fixes

--- solid-principles-tutorial/solid-principles-tutorial/Solid Principle/OpenClosedPrinciple.swift
+++ solid-principles-tutorial/solid-principles-tutorial/Solid Principle/OpenClosedPrinciple.swift
@@ -7,27 +7,54 @@
 
 import Foundation
 
-class APIService {
-    func fetch() -> String {
-        return "Data from API"
-    }
+//class APIService {
+//    func fetch() -> String {
+//        return "Data from API"
+//    }
+//}
+//
+//class DatabaseService {
+//    func fetch() -> String {
+//        return "Data from Database"
+//    }
+//}
+//
+//// Violation: Adding a new data source requires modifying DataFetcher → breaks OCP.
+//
+//class DataFetcher {
+//    func fetchData(source: String) -> String {
+//        if source == "api" {
+//            return APIService().fetch()
+//        } else if source == "db" {
+//            return DatabaseService().fetch()
+//        } else {
+//            return "Unknown source"
+//        }
+//    }
+//}
+
+
+// Fix Violation Above OCP
+
+protocol DataSource {
+    func fetch() -> String
 }
 
-class DatabaseService {
+class DatabaseService : DataSource {
     func fetch() -> String {
         return "Data from Database"
     }
 }
 
-class DataFetcher {
-    func fetchData(source: String) -> String {
-        if source == "api" {
-            return APIService().fetch()
-        } else if source == "db" {
-            return DatabaseService().fetch()
-        } else {
-            return "Unknown source"
-        }
+class APIService : DataSource {
+    func fetch() -> String {
+        return "Data from API"
     }
 }
 
+// get Data Source Without Changing Behaviour of Data Fetcher
+class DataFetcher {
+    func fetchData(source : DataSource) -> String {
+        source.fetch()
+    }
+}
